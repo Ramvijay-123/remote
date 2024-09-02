d27d49a12c47a1944d92908bad615ac9d5dfa101
12345rams <vijay90054442298@gmail.com>
2024-09-02 07:52:06 +0530

    adding new feature

--- frontend/src/apiService.js
+++ frontend/src/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:9000';
+const API_BASE_URL = 'https://remote-3.onrender.com/';
 
 export const registerUser = async (userDetails) => {
     try {
