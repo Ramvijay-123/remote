2cbfd9c1de74481784780037f2f760963731b5ce
Ram Vijay Yadav <132057037+12345rams@users.noreply.github.com>
2024-10-19 11:42:33 +0530

    Update KeepAliveService.java

--- backend/src/main/java/com/service/KeepAliveService.java
+++ backend/src/main/java/com/service/KeepAliveService.java
@@ -1,14 +1,23 @@
 package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 @Service
 public class KeepAliveService {
-    @Scheduled(fixedRate = 600000) 
+    private final String URL = "https://remote-theta.vercel.app/searchuser"; 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Scheduled(fixedRate = 6000)  
     public void pingMyApp() {
         try {
-           System.out.println("System is running");
+            String response = restTemplate.getForObject(URL, String.class); 
+            System.out.println("System is alive");  
         } catch (Exception e) {
-          
+            System.err.println("Error connecting to the backend: " + e.getMessage());
         }
     }
 }
